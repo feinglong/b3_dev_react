52b95d3325dc50201a3ad5b9f5f63d32e2f91cc5
Jeremie <feinglong@gmail.com>
2019-02-01 17:00:17 +0100

    add build

---
+++ build/precache-manifest.21e3f287318f0ea2e322d0779a59a23d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "933ca955e28c2620b7a9",
+    "url": "/static/js/main.933ca955.chunk.js"
+  },
+  {
+    "revision": "084f14b71467c17399f6",
+    "url": "/static/js/1.084f14b7.chunk.js"
+  },
+  {
+    "revision": "933ca955e28c2620b7a9",
+    "url": "/static/css/main.e7821fed.chunk.css"
+  },
+  {
+    "revision": "a56c389b0b78ab61e26b68c5f443e24d",
+    "url": "/index.html"
+  }
+];
